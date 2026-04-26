99187ae363348923a4e9a2d85779d930265cd46f
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2026-04-26 22:07:00 +0530

    Add files via upload

---
+++ program143.java
@@ -0,0 +1,40 @@
+/*
+A-------A
+bb-----bb
+CCC---CCC
+dddd-dddd
+EEEEEEEEE
+*/
+
+class program143
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			if(i==5)
+			{
+				for(int j=1;j<=(n*2)-1;j++)
+					System.out.print((char)(i+64));
+			}
+			else
+			{
+				for(int j=i;j>=1;j--)
+					if(i%2!=0)
+						System.out.print((char)(i+64));
+					else
+						System.out.print((char)(i+96));
+				for(int j=1;j<=((n-i)*2)-1;j++)
+					System.out.print("-");
+				for(int j=i;j>=1;j--)
+					if(i%2!=0)
+						System.out.print((char)(i+64));
+					else
+						System.out.print((char)(i+96));
+
+			}
+			System.out.println();
+		}
+	}
+}
